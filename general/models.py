9cc89766101f1d0bfb4ab62e33a38ca0e847d7a0
Boltuzamaki <divyanshuboltuzamaki23@gmail.com>
2020-10-17 01:57:47 +0530

    anime_added_to_database

--- general/models.py
+++ general/models.py
@@ -2,40 +2,22 @@ from django.db import models
 
 
 class Anime(models.Model):
-	ALL_SEASONS = [
-	("WINTER", 'Winter'),
-	("SPRING", 'Spring'),
-	("FALL", 'Fall'),
-	("SUMMER", 'Summer')]
-
-	SHOW_STATUS = [
-	("RUNNING", 'Running'),    
-	("COMPLETED", 'Completed')]
-
 	anime_id = models.IntegerField()
-	anime_name = models.CharField(max_length=100)
-	anime_synonyms = models.CharField(max_length=300)
-	anime_type = models.CharField(max_length=50)
-	anime_rank = models.IntegerField()
-	anime_score = models.DecimalField(max_digits=4, decimal_places=2)
-	anime_rating = models.CharField(max_length=100)
+	anime_name = models.CharField(max_length=100,default="Missing")
+	anime_synonyms = models.CharField(max_length=300, default="Missing")
+	anime_type = models.CharField(max_length=50, default="Missing")
+	anime_rank = models.CharField(max_length=8, default="Missing")
+	anime_score = models.CharField(max_length=5, default="Missing")
+	anime_rating = models.CharField(max_length=100,default="Missing")
 	anime_thumbnail = models.CharField(max_length=500)
 	anime_popularity = models.IntegerField(default=0)
 	total_members = models.IntegerField(default=0)
-	synopsis = models.CharField(max_length=1000)
-	genre = models.CharField(max_length=200)
+	genre = models.CharField(max_length=200,default="Missing")
 	total_episodes = models.IntegerField(default=0)
-	duration_min = models.DecimalField(max_digits=4, decimal_places=2)
-	studio = models.CharField(max_length=200)
-	start_season = models.CharField(
-		max_length=9,
-		choices=ALL_SEASONS,
-		default="WINTER",
-		)
-	start_year = models.IntegerField()
-	status = models.CharField(
-		max_length=9,
-		choices=SHOW_STATUS,
-		default="RUNNING",)
+	duration_min = models.CharField(max_length=8, default="Missing")
+	studio = models.CharField(max_length=200,default="Missing")
+	anime_premiered = models.CharField(max_length=10,default="Missing")
+	status = models.CharField(max_length=8,default="Missing")
+
 	def __str__(self):
 		return self.anime_name
--- general/views.py
+++ general/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render
 from .models import Anime
+import pandas as pd
 # Create your views here.
+
+"""
+# Add data from csv to database
+	tmp_data=pd.read_csv('C://Users//boltuzamaki//Desktop//animereco//aniReco//general//anime_cleaned.csv')
+	anime = [
+	Anime(
+    		anime_id = tmp_data.iloc[row]['anime_id'] ,
+    		anime_name = tmp_data.iloc[row]['title'] ,
+    		anime_synonyms = tmp_data.iloc[row]['title_synonyms'], 
+    		anime_type = tmp_data.iloc[row]['type'] ,
+    		anime_rank = tmp_data.iloc[row]['rank'] ,
+    		anime_score = tmp_data.iloc[row]['score'], 
+    		anime_rating = tmp_data.iloc[row]['rating'], 
+			anime_thumbnail = tmp_data.iloc[row]['image_url'], 
+			anime_popularity = tmp_data.iloc[row]['popularity'], 
+			total_members = tmp_data.iloc[row]['members'] ,
+			genre = tmp_data.iloc[row]['genre'] ,
+			total_episodes = tmp_data.iloc[row]['episodes'], 
+			duration_min = tmp_data.iloc[row]['duration_min'],
+			studio = tmp_data.iloc[row]['studio'] ,
+			anime_premiered = tmp_data.iloc[row]['premiered'],
+			status = tmp_data.iloc[row]['status'] 
+			)
+	for row in range(0, len(tmp_data))
+	]
+	print("Done lol")
+	Anime.objects.bulk_create(anime)
+
+	"""
+
 def home(request):
+
 	return render(request, 'general/home.html')
 
 def top_anime(request):
